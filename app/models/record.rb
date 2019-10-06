6b4fe93b53cef60cc3ea2d5e12c161130dd45147
Plum0207 <lapine.blanche.27@gmail.com>
2019-10-06 18:23:06 +0900

    making controller and models
    - making DB
    - defining associations among record, user, book
    - add validation
    - add routings
    - making records controller

---
+++ app/models/record.rb
@@ -0,0 +1,11 @@
+class Record < ApplicationRecord
+  belongs_to :book
+  belongs_to :user
+
+  with_options presence: true do
+    validates :date
+    validates :content
+    validates :amount
+    validates :wallet
+  end
+end
