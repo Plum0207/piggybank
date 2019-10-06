29240edc1b1c87e875ce86c84bc58149eddc9d9a
Plum0207 <lapine.blanche.27@gmail.com>
2019-10-06 13:19:32 +0900

    make book making function
    - add new and create action to books controller
    - add books/new page
    	- use form
    	- use collection_check_boxes
    - add scss for book pages
    - add books/index page as root
    - add flash messages
    - use Japanese as a default language

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,25 @@
 class BooksController < ApplicationController
   def index
-    
+    @books = current_user.books.order("title ASC")
   end
+
+  def new
+    @book = Book.new
+    @book.users << current_user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path, notice: '帳簿を作成しました'
+    else
+      render :new
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, {user_ids: []})
+  end
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   has_many :book_users
-  has_many :users, through: book_users
+  has_many :users, through: :book_users
 
   validates :title, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :users, presence: true
 end
