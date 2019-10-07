fd9223367d5ac37bd7365756eca2437ae20c6f6f
Plum0207 <lapine.blanche.27@gmail.com>
2019-10-07 22:53:41 +0900

    Add records#edit function
    - add edit button on records/index page
    - install gem 'font-awesome-rails'
    - ininstall gem 'font-awesome-sass'
    - add edit and update action to records controller
    - add edit page
    - use partial template for new and edit pages

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -19,12 +19,27 @@ class RecordsController < ApplicationController
     end
   end
 
+  def edit
+    @record = Record.find(params[:id])
+  end
+
+  def update
+    @record = Record.find(params[:id])
+    if @record.update(record_params)
+      redirect_to book_records_path(@book),notice: '記録を更新しました'
+    else
+      flash.now[:alert] = "記録を入力してください"
+      render :edit
+    end
+  end
+
+  private
   def record_params
     params.require(:record).permit(:date, :content, :amount, :wallet).merge(user_id: current_user.id)
   end
 
   def set_book
-    @book= Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
   end
 
 end
