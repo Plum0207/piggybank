a45b1325a51bc54fcfe38dd37a870e9aa2201018
Plum0207 <lapine.blanche.27@gmail.com>
2019-10-13 12:44:20 +0900

    Update categories_controller.rb
    - make functions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,23 @@
 class CategoriesController < ApplicationController
+  before_action :set_book
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
 end
