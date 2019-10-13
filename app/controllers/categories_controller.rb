7e0ad09865d2dd0be66545ff55321e9ebc2f26d1
Plum0207 <lapine.blanche.27@gmail.com>
2019-10-13 13:02:34 +0900

    make category making function
    - add new, create, index action to categories controller
    - add books/#/categories/new page
    - add books/#/categories page (index page)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,9 +5,17 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = @book.categories.new(category_params)
+    if @category.save
+      redirect_to book_categories_path(@book),
+      notice: 'カテゴリーを追加しました'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -17,6 +25,11 @@ class CategoriesController < ApplicationController
   end
 
   private
+
+  def category_params
+    params.require(:category).permit(:name, :budget)
+  end
+
   def set_book
     @book = Book.find(params[:book_id])
   end
