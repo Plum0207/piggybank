36fb113dc638e2cfd2c097f27c94e2abdb70fd0c
Plum0207 <lapine.blanche.27@gmail.com>
2019-10-19 14:54:24 +0900

    Update record model
    - use Japanese as titles of CSV file columns

--- app/models/record.rb
+++ app/models/record.rb
@@ -27,9 +27,13 @@ class Record < ApplicationRecord
     ["date", "content", "amount", "category", "wallet"]
   end
 
+  def self.csv_attributes_ja
+    ["日付", "内容", "金額", "費目", "支払者"]
+  end
+
   def self.generate_csv
     CSV.generate(headers: true) do |csv|
-      csv << csv_attributes
+      csv << csv_attributes_ja
       all.old.each do |record|
         csv << csv_attributes.map{ |attr| record.send(attr) }
       end
@@ -38,9 +42,14 @@ class Record < ApplicationRecord
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
-      record = new
-      record.attributes = row.to_hash.slice(*csv_attributes)
-      record.save!
+      row = row.to_hash.slice(*csv_attributes_ja)
+      self.create(
+        date: row["日付"],
+        content: row["内容"],
+        amount: row["金額"],
+        category: row["費目"],
+        wallet: row["支払者"]
+      )
     end
   end
 end
