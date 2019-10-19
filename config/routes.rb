305c3e99139dfe2d0736bb00677b30b282f54f04
Plum0207 <lapine.blanche.27@gmail.com>
2019-10-19 16:08:45 +0900

    Update routes
    - change root path depending on log-in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'books#index'
+  devise_scope :user do
+    authenticated :user do
+      root to: 'books#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+  
   resources :books, except: [:show] do
     resources :records, except: [:show] do
       post :import, on: :collection
