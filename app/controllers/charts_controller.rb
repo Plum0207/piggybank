600d78462b9aef2e05acb56837c44d2a8ed6524e
Plum0207 <lapine.blanche.27@gmail.com>
2019-10-20 12:47:10 +0900

    Create charts/index page
    - add detail of charts#index action
    - create charts/index.html.slim
    - add column chart

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,7 +1,28 @@
 class ChartsController < ApplicationController
+  before_action :set_book
 
   def index
+    @categories = @book.categories.all
+    @income = @categories.find_by(name:"収入")
+    @spending = @categories.where.not(name: "収入")
 
+    budget_data = {}
+    @spending.each do |category|
+      budget_data["#{category.name}"] = category.budget
+    end
+    budget = {name: "予算", data: budget_data}
+
+    amount_data = {}
+    @spending.each do |category|
+      amount_data["#{category.name}"] = category.records_amount(@book)
+    end
+    amount = {name: "実績", data: amount_data}
+
+    @column_chart_data = [budget, amount]
+  end
+
+  def set_book
+    @book = Book.find(params[:book_id])
   end
 
 end
