db5737fcbc7a230d77bb080b0aa81e822958fc6d
Plum0207 <lapine.blanche.27@gmail.com>
2019-10-20 13:10:18 +0900

    Update charts_controller
    - Refactoring
    - delete dupricated codes
    - modify index page accordingly

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -2,28 +2,20 @@ class ChartsController < ApplicationController
   before_action :set_book
 
   def index
-    @categories = @book.categories.all
-    @income = @categories.find_by(name:"収入")
-    @spending = @categories.where.not(name: "収入")
+    @spending = @book.categories.where.not(name: "収入")
 
     budget_data = {}
+    @amount_data = {}
+
     @spending.each do |category|
       budget_data["#{category.name}"] = category.budget
+      @amount_data["#{category.name}"] = category.records_amount(@book)
     end
-    budget = {name: "予算", data: budget_data}
 
-    amount_data = {}
-    @spending.each do |category|
-      amount_data["#{category.name}"] = category.records_amount(@book)
-    end
-    amount = {name: "実績", data: amount_data}
-
-    @column_chart_data = [budget, amount]
-
-    @pie_chart_data = {}
-    @spending.each do |category|
-      @pie_chart_data["#{category.name}"] = category.records_amount(@book)
-    end
+    @column_chart_data = [ 
+      {name: "予算", data: budget_data},
+      {name: "実績", data: @amount_data}
+    ]
   end
 
   def set_book
