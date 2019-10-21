892d06c48829fbe701f3d416d2b85d9350dd9386
Plum0207 <lapine.blanche.27@gmail.com>
2019-10-21 14:53:34 +0900

    Update factories/books

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :book do
     title {"サンプル"}
     users {[FactoryBot.build(:user)]}
+    created_at { Faker::Time.between_dates(from: Date.today - 1, to: Date.today)}
   end
 end
